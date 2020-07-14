25175e7eb3b102a3900d54e690cb75d151fc0d20
how <li_zihao2015@163.com>
2020-07-14 21:46:13 +0800

    作业二

--- testing/conftest.py
+++ testing/conftest.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+add_data = [
+    (1, 2, 3),
+    (1.1, 2.4, 3.5),
+    (-2, -1, -3),
+    (-2, 1, -1),
+    (0, 3, 3)
+]
 
-@pytest.fixture(autouse=True)
-def calconf():
+
+@pytest.fixture(scope='function', params=add_data)
+def addconf(request):
+    data = request.param
     print('开始计算')
-    yield
+    yield data
     print('\n计算结束')
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -4,75 +4,57 @@
 import pytest
 import sys
 
+import yaml
+
 sys.path.append('../')
 from pythoncode.calc import Calculator
+
+with open("datas/calc.yml") as f:
+    datas = yaml.safe_load(f)
+    # 取出加法测试数据
+    add_data = datas['add']
+    add_ids = add_data.keys()
+    add_datas = add_data.values()
+    # 取出减法测试数据
+    dec_data = datas['dec']
+    dec_ids = dec_data.keys()
+    dec_datas = dec_data.values()
+    # 取出除法测试数据
+    div_data = datas['div']
+    div_ids = div_data.keys()
+    div_datas = div_data.values()
+    # 取出乘法测试数据
+    mcl_data = datas['mcl']
+    mcl_ids = mcl_data.keys()
+    mcl_datas = mcl_data.values()
 
 
 # 计算器测试类
 class TestCalc:
+
     def setup(self):
         self.cal = Calculator()
 
     # 加法测试方法
     @pytest.mark.add
-    @pytest.mark.parametrize('a, b, result', [
-        (1, 2, 3),
-        (1.1, 2.4, 3.5),
-        (-2, -1, -3),
-        (-2, 1, -1),
-        (0, 3, 3)
-    ], ids=['int',
-            'float',
-            'negative',
-            'negative&int',
-            'zero'])
-    def test_add(self, a, b, result):
+    @pytest.mark.parametrize('a, b, result', add_datas, ids=add_ids)
+    def check_add(self, a, b, result):
         assert result == self.cal.add(a, b)
 
     # 减法测试方法
     @pytest.mark.dec
-    @pytest.mark.parametrize('a, b, result', [
-        (4, 2, 2),
-        (2.5, 1.2, 1.3),
-        (-2, -1, -1),
-        (-2, 1, -3),
-        (3, 0, 3)
-    ], ids=['int',
-            'float',
-            'negative',
-            'negative&int',
-            'zero'])
-    def test_dec(self, a, b, result):
+    @pytest.mark.parametrize('a, b, result', dec_datas, ids=dec_ids)
+    def check_dec(self, a, b, result):
         assert result == self.cal.dec(a, b)
 
     # 乘法测试方法
     @pytest.mark.mcl
-    @pytest.mark.parametrize('a, b, result', [
-        (2, 3, 6),
-        (2.2, 1.2, 2.64),
-        (-2, -3, 6),
-        (3, -2, -6),
-        (0, 4, 0)
-    ], ids=['int',
-            'float',
-            'negative',
-            'negative&int',
-            'zero'])
-    def test_mcl(self, a, b, result):
+    @pytest.mark.parametrize('a, b, result', mcl_datas, ids=mcl_ids)
+    def check_mcl(self, a, b, result):
         assert result == self.cal.mcl(a, b)
 
     # 除法测试方法
     @pytest.mark.div
-    @pytest.mark.parametrize('a, b, result', [
-        (6, 3, 2),
-        (2.64, 2.2, 1.2),
-        (-6, -2, 3),
-        (-6, 2, -3),
-        (0, 4, 0)
-    ], ids=['int',
-            'float',
-            'negative',
-            'negative&int',
-            'zero'])
-    def test_div(self, a, b, result):
+    @pytest.mark.parametrize('a, b, result', div_datas, ids=div_ids)
+    def check_div(self, a, b, result):
         assert result == self.cal.div(a, b)
